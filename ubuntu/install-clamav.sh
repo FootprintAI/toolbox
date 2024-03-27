527064e6383ae66fc226a1a940b5a2ccf9c2ef19
hsinhoyeh <yhh92u@gmail.com>
2024-03-27 13:29:11 +0800

    update script

--- ubuntu/install-clamav.sh
+++ ubuntu/install-clamav.sh
@@ -6,21 +6,16 @@ if (( $EUID != 0 )); then
    exit
 fi
 
-if [ -z "$1" ]
-  then
-    echo "require notification email"
-    exit
-fi
-
 apt-get update && \
     apt-get install cron clamav clamav-daemon -y
 
 systemctl enable clamav-daemon
 systemctl enable clamav-freshclam
 
+mkdir /var/log/antivirus
+
 # setup crontab job for automatic scanning daily and email reports
 crontab -l > mycron
-echo "MAILTO=$1" >> mycron
-echo "03 3 * * * /usr/bin/clamscan -ri --no-summary /" >> mycron
+echo "03 3 * * * /usr/bin/clamscan -ri --no-summary / 2&1 >> /var/log/antivirus/antivus.log" >> mycron
 crontab mycron
 rm mycron
