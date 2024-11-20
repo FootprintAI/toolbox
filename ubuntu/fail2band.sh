d968c8ddad9394ef301966d76557321c196061d4
hsinatfootprintai <149764663+hsinatfootprintai@users.noreply.github.com>
2024-11-21 06:28:21 +0800

    Create fail2band.sh
    for bading invalid auth request via ssh

---
+++ ubuntu/fail2band.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# run as root
+if (( $EUID != 0 )); then
+   echo "this script should be running as root identity"
+   exit
+fi
+
+apt-get install -y fail2ban
+
+# copy configures
+cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+# check services 
+systemctl status fail2ban
+
+### check ban status
+fail2ban-client status sshd
+
+# Status for the jail: sshd
+#|- Filter
+#|  |- Currently failed: 1
+#|  |- Total failed:     6
+#|  `- File list:        /var/log/auth.log
+#`- Actions
+#   |- Currently banned: 1
+#   |- Total banned:     1
+#   `- Banned IP list:   xxx.yyy.zzzz.hhh
